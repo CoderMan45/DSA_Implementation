c5c772bb742844288dec2aba02b21e647518d7cc
Uttiya <iamuttiyapal@gmail.com>
2024-07-27 03:20:19 +0530

    Updated binary_search_tree.c

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node {
     struct node* lchild;
@@ -6,7 +7,29 @@ struct node {
     int data;
 };
 
+struct node* newNode(struct node* ptr, int val){
+    if (ptr==NULL){
+        ptr = (struct node*)malloc(sizeof(struct node));
+        ptr->lchild = NULL;
+        ptr->rchild = NULL;
+        ptr->data = val;
+        return ptr;
+    }
+    if (ptr->data>val){
+        newNode(ptr->lchild,val);
+    }
+    else if (ptr->data<val) 
+        newNode(ptr->rchild,val);
+}
+
 int main(){
     struct node* root = NULL;
-    root = (struct node*)malloc(sizeof(struct node));
+    root = newNode(root,50);
+    newNode(root,14);
+    newNode(root,86);
+    newNode(root,40);
+    newNode(root,70);
+    newNode(root,24);
+    newNode(root,51);
+    newNode(root,23);
 }
